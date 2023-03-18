ee5a777a27fcbba80b4666e6b98916da96da232c
Raphael blaise <44173514+Raphael-Blaize@users.noreply.github.com>
2023-03-18 14:08:43 +0300

    Create

---
+++ say.py
@@ -0,0 +1,13 @@
+#importing sys for the command-line arguments
+import sys
+
+
+#importing from the sayin library g the function hello
+from saying import hello
+
+#checking if the user has provided two arguments at the command line
+if len(sys.argv) == 2:
+  #printing the argument at location 1
+    hello(sys.argv[1])
+else:
+  sys.exit
